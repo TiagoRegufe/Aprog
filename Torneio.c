8f2c4ab0d1306e929254e169e6dea3d102aa289f
TiagoRegufe <92099227+TiagoRegufe@users.noreply.github.com>
2022-01-02 19:41:57 +0000

    Update Torneio.c

--- Torneio.c
+++ Torneio.c
@@ -26,37 +26,37 @@ void Equipas()  //ponto 2 da parte 1
 
     //escreve o nome das equipas
 
-    strcpy(NEQS[0].equipa, "Arouca");
-    strcpy(NEQS[1].equipa, "Belenenses");
-    strcpy(NEQS[2].equipa, "Benfica");
-    strcpy(NEQS[3].equipa, "Boavista");
-    strcpy(NEQS[4].equipa, "Braga");
-    strcpy(NEQS[5].equipa, "Estoril");
-    strcpy(NEQS[6].equipa, "Famalicao");
-    strcpy(NEQS[7].equipa, "Gil Vicente");
-    strcpy(NEQS[8].equipa, "Maritimo");
-    strcpy(NEQS[9].equipa, "Moreirense");
-    strcpy(NEQS[10].equipa, "Pacos Ferreira");
-    strcpy(NEQS[11].equipa, "Porto");
-    strcpy(NEQS[12].equipa, "Portimonense");
-    strcpy(NEQS[13].equipa, "Santa Clara");
-    strcpy(NEQS[14].equipa, "Sporting");
-    strcpy(NEQS[15].equipa, "Tondela");
-    strcpy(NEQS[16].equipa, "Vitoria");
-    strcpy(NEQS[17].equipa,  "Vizela");
+    strcpy(NEQS[0].equipa, "Arouca        ");
+    strcpy(NEQS[1].equipa, "Belenenses    ");
+    strcpy(NEQS[2].equipa, "Benfica       ");
+    strcpy(NEQS[3].equipa, "Boavista      ");
+    strcpy(NEQS[4].equipa, "Braga         ");
+    strcpy(NEQS[5].equipa, "Estoril       ");
+    strcpy(NEQS[6].equipa, "Famalicao     ");
+    strcpy(NEQS[7].equipa, "Gil Vicente   ");
+    strcpy(NEQS[8].equipa, "Maritimo      ");
+    strcpy(NEQS[9].equipa, "Moreirense    ");
+    strcpy(NEQS[10].equipa,"Pacos Ferreira");
+    strcpy(NEQS[11].equipa,"Porto         ");
+    strcpy(NEQS[12].equipa,"Portimonense  ");
+    strcpy(NEQS[13].equipa,"Santa Clara   ");
+    strcpy(NEQS[14].equipa,"Sporting      ");
+    strcpy(NEQS[15].equipa,"Tondela       ");
+    strcpy(NEQS[16].equipa,"Vitoria       ");
+    strcpy(NEQS[17].equipa,"Vizela        ");
 
     //escreve os estádios
 
-    strcpy(NEQS[0].estadio, "Estadio Municipal de Arouca");
-    strcpy(NEQS[1].estadio, "Estadio do Restelo");
-    strcpy(NEQS[2].estadio, "Estadio da Luz");
-    strcpy(NEQS[3].estadio, "Estadio do Bessa");
-    strcpy(NEQS[4].estadio, "Estadio Municipal de Braga");
-    strcpy(NEQS[5].estadio, "Estadio Antonio Coimbra da Mota");
-    strcpy(NEQS[6].estadio, "Estadio Municipal 22 de Junho");
-    strcpy(NEQS[7].estadio, "Estadio Cidade de Barcelos");
-    strcpy(NEQS[8].estadio, "Estadio dos Barreiros");
-    strcpy(NEQS[9].estadio, "Estadio Comendador Joaquim de Almeida Freitas");
+    strcpy(NEQS[0].estadio,  "Estadio Municipal de Arouca");
+    strcpy(NEQS[1].estadio,  "Estadio do Restelo");
+    strcpy(NEQS[2].estadio,  "Estadio da Luz");
+    strcpy(NEQS[3].estadio,  "Estadio do Bessa");
+    strcpy(NEQS[4].estadio,  "Estadio Municipal de Braga");
+    strcpy(NEQS[5].estadio,  "Estadio Antonio Coimbra da Mota");
+    strcpy(NEQS[6].estadio,  "Estadio Municipal 22 de Junho");
+    strcpy(NEQS[7].estadio,  "Estadio Cidade de Barcelos");
+    strcpy(NEQS[8].estadio,  "Estadio dos Barreiros");
+    strcpy(NEQS[9].estadio,  "Estadio Comendador Joaquim de Almeida Freitas");
     strcpy(NEQS[10].estadio, "Estadio da Capital Movel");
     strcpy(NEQS[11].estadio, "Estadio do Dragao");
     strcpy(NEQS[12].estadio, "Estadio do Portimonense");
@@ -64,10 +64,11 @@ void Equipas()  //ponto 2 da parte 1
     strcpy(NEQS[14].estadio, "Estadio Jose Alvalade");
     strcpy(NEQS[15].estadio, "Estadio Joao Cardoso");
     strcpy(NEQS[16].estadio, "Estadio Dom Afonso Henriques");
-    strcpy(NEQS[17].estadio,  "Estadio do Vizela");
+    strcpy(NEQS[17].estadio, "Estadio do Vizela");
 
     //zerar pontos, vitórias, empates, derrotas, golos
 int n=18;
+printf("                   P  V  E  D  M  S \n");
     for(int i=0;i<n;i++){
         NEQS[i].pontos= 0;
         NEQS[i].vitorias= 0;
@@ -76,6 +77,7 @@ int n=18;
         NEQS[i].golos_marcados= 0;
         NEQS[i].golos_sofridos= 0;
 //Escrever as equipas e a pontuaçao a zero
+
         printf("%s     %d  %d  %d  %d  %d  %d",NEQS[i].equipa,NEQS[i].pontos,NEQS[i].vitorias,NEQS[i].empates,NEQS[i].derrotas,NEQS[i].golos_marcados,NEQS[i].golos_sofridos);
         printf("\n");
         }
@@ -92,3 +94,4 @@ int main()
   getchar();
   return 0;
 }
+
