2efbe75ac4ee242d571c191fe917641f9e160efe
Gustavo Fialho <91161932+gustavof05@users.noreply.github.com>
2022-01-03 22:13:50 +0000

    Update Torneio.c

--- Torneio.c
+++ Torneio.c
@@ -4,8 +4,7 @@
 #include<math.h>
 #include<string.h>
 
-typedef  //ponto 1 da parte 1
-struct s_equipa
+typedef struct s_equipa     //ponto 1 da parte 1
 {
     char equipa[15];
     char estadio[46];
@@ -15,17 +14,13 @@ struct s_equipa
     int derrotas;
     int golos_marcados;
     int golos_sofridos;
-
 } t_equipa;
 
-
-void Equipas()  //ponto 2 da parte 1
+void Equipas()              //ponto 2 da parte 1
 {
-    struct s_equipa equipa1,equipa2,equipa3,equipa4,equipa5,equipa6,equipa7,equipa8,equipa9,equipa10,equipa11,equipa12,equipa13,equipa14,equipa15,equipa16,equipa17,equipa18;
-    t_equipa NEQS[18]={equipa1,equipa2,equipa3,equipa4,equipa5,equipa6,equipa7,equipa8,equipa9,equipa10,equipa11,equipa12,equipa13,equipa14,equipa15,equipa16,equipa17,equipa18};
-
+    struct s_equipa equipa1, equipa2, equipa3, equipa4, equipa5, equipa6, equipa7, equipa8, equipa9, equipa10, equipa11, equipa12, equipa13, equipa14, equipa15, equipa16, equipa17, equipa18;
+    t_equipa NEQS[18]={equipa1, equipa2, equipa3, equipa4, equipa5, equipa6, equipa7, equipa8, equipa9, equipa10, equipa11, equipa12, equipa13, equipa14, equipa15, equipa16, equipa17, equipa18};
     //escreve o nome das equipas
-
     strcpy(NEQS[0].equipa, "Arouca        ");
     strcpy(NEQS[1].equipa, "Belenenses    ");
     strcpy(NEQS[2].equipa, "Benfica       ");
@@ -44,9 +39,7 @@ void Equipas()  //ponto 2 da parte 1
     strcpy(NEQS[15].equipa,"Tondela       ");
     strcpy(NEQS[16].equipa,"Vitoria       ");
     strcpy(NEQS[17].equipa,"Vizela        ");
-
     //escreve os estádios
-
     strcpy(NEQS[0].estadio,  "Estadio Municipal de Arouca");
     strcpy(NEQS[1].estadio,  "Estadio do Restelo");
     strcpy(NEQS[2].estadio,  "Estadio da Luz");
@@ -65,11 +58,10 @@ void Equipas()  //ponto 2 da parte 1
     strcpy(NEQS[15].estadio, "Estadio Joao Cardoso");
     strcpy(NEQS[16].estadio, "Estadio Dom Afonso Henriques");
     strcpy(NEQS[17].estadio, "Estadio do Vizela");
-
     //zerar pontos, vitórias, empates, derrotas, golos(tem que ser uma funcao)
-int n=18;
-
-    for(int i=0;i<n;i++){
+    int n=18;
+    for(int i=0;i<n;i++)
+    {
         NEQS[i].pontos= 0;
         NEQS[i].vitorias= 0;
         NEQS[i].empates= 0;
@@ -77,25 +69,23 @@ int n=18;
         NEQS[i].golos_marcados= 0;
         NEQS[i].golos_sofridos= 0;
     //Escrever a tabela(tem que ser outra funcao)
-
-        printf("%s     %.2d  %.2d  %.2d  %.2d  %.2d  %.2d",NEQS[i].equipa,NEQS[i].pontos,NEQS[i].vitorias,NEQS[i].empates,NEQS[i].derrotas,NEQS[i].golos_marcados,NEQS[i].golos_sofridos);
-        printf("\n");
+        printf("%s     %.2d  %.2d  %.2d  %.2d  %.2d  %.2d\n",NEQS[i].equipa,NEQS[i].pontos,NEQS[i].vitorias,NEQS[i].empates,NEQS[i].derrotas,NEQS[i].golos_marcados,NEQS[i].golos_sofridos);
         }
 }
-void trocar_elementos(NEQS[]){
+
+void trocar_elementos(NEQS[])
+{
     
 }
-
-
 
 int main()
 {
     int n=18;
     printf("                   P   V   E   D   M   S \n");
     Equipas();
-
-  getchar();
-  return 0;
+    
+    getchar();
+    return 0;
 }
 
 
